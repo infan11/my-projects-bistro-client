ba693ff6a5352480e4659e036c063545e826bfba
infan11 <infanjiounrahman20606@gmail.com>
2024-02-08 22:51:13 +0600

    added vercel link

--- src/Components/Hooks/UseMenu.jsx
+++ src/Components/Hooks/UseMenu.jsx
@@ -5,7 +5,7 @@ const UseMenu = () => {
     const [menu , setMenu] = useState([]);
     const [loading , setLading] = useState(true);
     useEffect( () =>  {
-        fetch("http://localhost:5000/menu")
+        fetch("https://bistro-boss-server-projects-aroi6m9ee-infan11s-projects.vercel.app/menu")
         .then(res => res.json())
         .then(data => {
             setMenu(data);
